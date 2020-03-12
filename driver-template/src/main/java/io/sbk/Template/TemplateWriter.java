da0780f678a566aa34881a9c7aa318df5f837132
Keshava Munegowda <keshava.gowda@gmail.com>
2020-03-12 12:07:08 +0530

    Template Driver Implementation
    Signed-off-by: Keshava Munegowda <keshava.gowda@gmail.com>

---
+++ driver-template/src/main/java/io/sbk/Template/TemplateWriter.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) KMG. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.sbk.Template;
+import io.sbk.api.Writer;
+import io.sbk.api.Parameters;
+
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+
+
+/**
+ * Class for Writer.
+ */
+public class TemplateWriter implements Writer<byte[]> {
+
+    public TemplateWriter(int writerID, Parameters params) {
+    }
+
+    @Override
+    public CompletableFuture writeAsync(byte[] data) throws IOException {
+        throw new IOException("Its Template Writer Driver");
+    }
+
+
+    @Override
+    public void flush() throws IOException {
+        throw new IOException("Its Template Writer Driver");
+    }
+
+    @Override
+    public void close() throws IOException {
+        throw new IOException("Its Template Writer Driver");
+    }
+}
