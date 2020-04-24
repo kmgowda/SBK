17edc58e13e903bbd9d15704335b07b96725ec45
Keshava Munegowda <keshava.gowda@gmail.com>
2020-04-24 15:52:58 +0530

    Cleanup.
    Signed-off-by: Keshava Munegowda <keshava.gowda@gmail.com>

--- config/kubernetes/sbk-rabbitmq-k8-sample.sh
+++ config/kubernetes/sbk-rabbitmq-k8-sample.sh
@@ -5,6 +5,3 @@ kubectl delete -f ${DIR}/sbk-rabbitmq-k8-sample.yaml -n ${NAMESPACE}
 kubectl apply -f ${DIR}/sbk-rabbitmq-k8-sample.yaml -n ${NAMESPACE}
 sleep 5s
 kubectl logs -f jobs/sbk-rabbitmq-k8 -n ${NAMESPACE}
-
-
-
