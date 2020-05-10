d53174ff23c05465573e225b8b5508344f56cf04
Keshava Munegowda <keshava.gowda@gmail.com>
2020-05-10 20:30:06 +0530

    Fix the comments.
    Signed-off-by: Keshava Munegowda <keshava.gowda@gmail.com>

--- sbk-api/src/main/java/io/sbk/api/Storage.java
+++ sbk-api/src/main/java/io/sbk/api/Storage.java
@@ -88,7 +88,7 @@ public interface Storage<T> {
      * If your Benchmark data type is other than byte[] then you need to implement your own Data class.
      * If the data type of your Benchmark, Reader and Writer classes  is byte[] (Byte Array),
      * then use this default implementation as it is.
-     * @return Data Data interface, null in case of failure
+     * @return Data Data interface.
      * @throws IllegalArgumentException if data type is other than byte[]
      */
     default DataType<?> getDataType() throws IllegalArgumentException {
