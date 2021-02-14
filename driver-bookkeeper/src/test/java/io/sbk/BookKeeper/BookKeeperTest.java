db649e308b6be5997a8bd405501bf4441d7cbc03
sanjaynv <sanjaynv@gmail.com>
2021-02-15 00:12:42 +0530

    Fix check-style errors
    Signed-off-by: sanjaynv <sanjaynv@gmail.com>

--- driver-bookkeeper/src/test/java/io/sbk/BookKeeper/BookKeeperTest.java
+++ driver-bookkeeper/src/test/java/io/sbk/BookKeeper/BookKeeperTest.java
@@ -1,4 +1,3 @@
-
 /**
  * Copyright (c) KMG. All Rights Reserved.
  *
@@ -9,32 +8,42 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  */
 package io.sbk.BookKeeper;
+
 import com.beust.jcommander.internal.Lists;
-import io.sbk.api.impl.SbkParameters;
 import io.sbk.api.Parameters;
+import io.sbk.api.impl.SbkParameters;
 import org.junit.Test;
+import java.util.ArrayList;
 import java.util.Hashtable;
-import java.util.*;
+import java.util.List;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
 
 //create the test Class for Bookkeeper Benchmarking
 public class BookKeeperTest {
-    long startTime = System.currentTimeMillis();
+    /**.
+     *  * Initializing variable        */
+    private final static String CONFIGFILE = "BookKeeper.properties";
+    private long startTime = System.currentTimeMillis();
     // test list of driver strings
-    final List<String> driverList = Lists.newArrayList("bookkeeper", "rabbitmq", "hdfs");
-    String version = "0.7";
-    String benchmarkName = "BookkeeperBench";
-    String className = "BookKeeperClass";
-    String[] test_cmd_args = {
-            "-log","abcd","-uri","testuri","-ensembleSize", "1",
-            "-writeQuorum", "1", "-ackQuorum", "1",
-            "-recreate","True","-writers", "1", "-readers", "1","-size","8"};    
+    /** Adding Driver List. */
+    private final List<String> driverList;
+
+    {
+        driverList = Lists.newArrayList("bookkeeper", "rabbitmq", "hdfs");
+    }
+    /**
+     *  * Initialization version benchmarkName className. */
+    private String version = "0.7";
+    private String benchmarkName = "BookkeeperBench";
+    private String className = "BookKeeperClass";
+    private String[] testCmdArgs = new String[]{
+                "-log", "abcd", "-uri", "testuri", "-ensembleSize", "1",
+                "-writeQuorum", "1", "-ackQuorum", "1",
+                "-recreate", "True", "-writers", "1", "-readers", "1", "-size", "8"};
 
     // test_args holds test keys and values so that later
     // we can verify on params
-    private final static String CONFIGFILE = "BookKeeper.properties";
+
     private org.apache.distributedlog.api.namespace.Namespace namespace;
     private java.net.URI dlogUri;
     private org.apache.distributedlog.DistributedLogConfiguration conf;
@@ -46,35 +55,50 @@ public class BookKeeperTest {
     private int writeQuorum;
     private int ackQuorum;
 
+    /** BookKeeperTest Method.
+     */
+    public BookKeeperTest() {
+
+    }
+
     @Test
     public void addArgsTest() throws Exception {
-        Hashtable<String, String> test_args = new Hashtable<String, String>();
+        Hashtable<String, String> testArgs = new Hashtable<String, String>();
         List<String> listStrings = new ArrayList<String>();
         driverList.add("bookkeeper bench");
-        Parameters params = new SbkParameters(benchmarkName,driverList);
+        Parameters params = new SbkParameters(benchmarkName, driverList);
         params.addOption("log", true, "Log name");
         params.addOption("uri", true, "URI");
-        params.addOption("ensembleSize", true, "EnsembleSize (default value is in "+CONFIGFILE+" )");
-        params.addOption("writeQuorum", true, "WriteQuorum  (default value is in "+CONFIGFILE+" )");
-        params.addOption("ackQuorum", true, "AckQuorum (default value is in "+CONFIGFILE+" )");
+        params.addOption("ensembleSize", true,
+                "EnsembleSize(default value is in " + CONFIGFILE + ")");
+        params.addOption("writeQuorum", true,
+                "WriteQuorum(default value is in " + CONFIGFILE + " )");
+        params.addOption("ackQuorum", true,
+                "AckQuorum(default value is in " + CONFIGFILE + " )");
         params.addOption("recreate", true,
-                "If the log is already existing, delete and recreate the same (default: false)");
+                "If the log is already existing, delete and recreate the same");
         BookKeeper bk = new BookKeeper();
         bk.addArgs(params);
 
     }
+    /**
+    Test code for parseArgs.
+     */
     @Test
     public void parseArgs() throws Exception {
         List<String> listStrings = new ArrayList<String>();
         driverList.add("bookkeeper bench");
-        Parameters params = new SbkParameters(benchmarkName,driverList);
+        Parameters params = new SbkParameters(benchmarkName, driverList);
         params.addOption("log", true, "Log name");
         params.addOption("uri", true, "URI");
-        params.addOption("ensembleSize", true, "EnsembleSize (default value is in "+CONFIGFILE+" )");
-        params.addOption("writeQuorum", true, "WriteQuorum  (default value is in "+CONFIGFILE+" )");
-        params.addOption("ackQuorum", true, "AckQuorum (default value is in "+CONFIGFILE+" )");
+        params.addOption("ensembleSize", true,
+                "EnsembleSize (default value is in " + CONFIGFILE + " )");
+        params.addOption("writeQuorum", true,
+                "WriteQuorum  (default value is in " + CONFIGFILE + " )");
+        params.addOption("ackQuorum", true,
+                "AckQuorum (default value is in " + CONFIGFILE + " )");
         params.addOption("recreate", true,
-                "If the log is already existing, delete and recreate the same (default: false)");
+                "If the log is already exist, delete and recreate the same)");
         BookKeeper bk = new BookKeeper();
         // doThrow(IllegalArgumentException.class).when(params).parseArgs(any());
         Exception exception = null;
@@ -84,5 +108,6 @@ public class BookKeeperTest {
             exception = e;
         }
         assertNotNull(exception);
+
     }
 }
