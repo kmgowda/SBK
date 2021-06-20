d1bb58c4b871e2e75e36fccea71488c09840aefe
Keshava Munegowda <keshava.gowda@gmail.com>
2021-06-20 13:30:49 +0530

    Restructure the command options.
    Signed-off-by: Keshava Munegowda <keshava.gowda@gmail.com>

--- sbk-api/src/main/java/io/sbk/api/impl/SbkOptions.java
+++ sbk-api/src/main/java/io/sbk/api/impl/SbkOptions.java
@@ -53,29 +53,17 @@ public class SbkOptions implements InputOptions {
 
     @Override
     public boolean hasOption(String name) {
-        if (commandline != null) {
-            return commandline.hasOption(name);
-        } else {
-            return false;
-        }
+        return commandline != null && commandline.hasOption(name);
     }
 
     @Override
     public String getOptionValue(String name) {
-        if (commandline != null) {
-            return commandline.getOptionValue(name);
-        } else {
-            return null;
-        }
+        return commandline != null ? commandline.getOptionValue(name) : null;
     }
 
     @Override
     public String getOptionValue(String name, String defaultValue) {
-        if (commandline != null) {
-            return commandline.getOptionValue(name, defaultValue);
-        } else {
-            return defaultValue;
-        }
+        return commandline != null ? commandline.getOptionValue(name, defaultValue) : defaultValue;
     }
 
     @Override
