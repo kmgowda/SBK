6123d468a45c0224d3a3dfde9b59e66b6cad0492
TheCharmingSociopath <aditya.morolia@research.iiit.ac.in>
2021-07-13 12:21:19 +0530

    Add parameters to write to stdout and to pass custom csvfile name

--- sbk-api/src/main/java/io/sbk/api/impl/SbkCSVLogger.java
+++ sbk-api/src/main/java/io/sbk/api/impl/SbkCSVLogger.java
@@ -11,21 +11,45 @@
 package io.sbk.api.impl;
 
 import java.io.PrintWriter;
+
+import io.sbk.api.InputOptions;
+
 import java.io.FileWriter;
 
 /**
  * Class for recoding/printing results to a CSV file called `out.csv`.
  */
 public class SbkCSVLogger extends SystemLogger {
-    public String fileName = "out.csv";
+    private String fileName;
+    private boolean writeToStdout;
+    private PrintWriter printWriter;
 
     public SbkCSVLogger() {
         super();
+    }
+
+    @Override
+    public void addArgs(final InputOptions params) throws IllegalArgumentException {
+        super.addArgs(params);
+        params.addOption("csvfile", true, "CSV file name to append the metrics to, e.g.: myOutput.csv; defalt: out.csv");
+        params.addOption("stdout", false, "Write the metrics to stdout along with the csv file.");
+    }
+
+    @Override
+    public void parseArgs(final InputOptions params) throws IllegalArgumentException {
+        super.parseArgs(params);
+        fileName = params.getOptionValue("csvfile", "out.csv");
+        if (params.hasOption("stdout")) {
+            writeToStdout = true;
+        } else {
+            writeToStdout = false;
+        }
+
+        // Writing the header to the csv file.
         String header = "Prefix, Writers, Readers, maxWriters, maxReaders, Bytes, Records, RecsPerSec, mbPerSec, AvgLatency, maxLatency, InvalidLatencies, lowerDiscard, higherDiscard, 10thPercentile, 25thPercentile, 50thPercentile, 75thPercentile, 90thPercentile, 95thPercentile, 99thPercentile, 99.9thPercentile, 99.99thPercentile\n";
         try {
-            PrintWriter pw = new PrintWriter(fileName);
-            pw.print(header);
-            pw.close();
+            printWriter = new PrintWriter(new FileWriter(fileName, true));
+            printWriter.print(header);
         } catch (Exception e) {
             e.getStackTrace();
         }
@@ -39,9 +63,7 @@ public class SbkCSVLogger extends SystemLogger {
             data += String.format(", %7d", percentileValues[i]);
         }
         try {
-            PrintWriter pw = new PrintWriter(new FileWriter(fileName, true));
-            pw.println(data);
-            pw.close();
+            printWriter.println(data);
         } catch (Exception e) {
             e.getStackTrace();
         }
@@ -50,6 +72,9 @@ public class SbkCSVLogger extends SystemLogger {
     @Override
     public void print(long bytes, long records, double recsPerSec, double mbPerSec, double avgLatency,
                       long maxLatency, long invalid, long lowerDiscard, long higherDiscard, long[] percentileValues) {
+        if (writeToStdout) {
+            super.print(bytes, records, recsPerSec, mbPerSec, avgLatency, maxLatency, invalid, lowerDiscard, higherDiscard, percentileValues);
+        }
         writeToCSV(prefix, bytes, records, recsPerSec, mbPerSec, avgLatency, maxLatency, invalid, lowerDiscard,
                 higherDiscard, percentileValues);
     }
@@ -57,7 +82,11 @@ public class SbkCSVLogger extends SystemLogger {
     @Override
     public void printTotal(long bytes, long records, double recsPerSec, double mbPerSec, double avgLatency,
                       long maxLatency, long invalid, long lowerDiscard, long higherDiscard, long[] percentilesValues) {
+        if (writeToStdout) {
+            super.printTotal(bytes, records, recsPerSec, mbPerSec, avgLatency, maxLatency, invalid, lowerDiscard, higherDiscard, percentilesValues);
+        }
         writeToCSV("Total : " + prefix, bytes, records, recsPerSec, mbPerSec, avgLatency, maxLatency,
                 invalid, lowerDiscard, higherDiscard, percentilesValues);
+        printWriter.close();
     }
 }
