4ea3c2477e3ce63fa74320eefa0f5594313c8ee5
Keshava Munegowda <keshava.gowda@gmail.com>
2021-10-24 15:47:32 +0530

    Jipack setup
    Signed-off-by: Keshava Munegowda <keshava.gowda@gmail.com>

--- jitpack_setup.sh
+++ jitpack_setup.sh
@@ -1,2 +1,15 @@
-echo "KMG"
-echo $JAVA_HOME
+#
+# Copyright (c) KMG. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+
+echo "Jit pack setup for SBK"
+echo "Default JAVA_HOME :$JAVA_HOME"
+wget https://github.com/sormuras/bach/raw/master/install-jdk.sh
+source install-jdk.sh -F 17 -L GPL
+echo "Updated JAVA_HOME :$JAVA_HOME"
