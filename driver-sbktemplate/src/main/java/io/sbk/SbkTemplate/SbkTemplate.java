fcf667678a293e42e17e97ea3c35fa10ab0f46c9
Keshava Munegowda <keshava.gowda@gmail.com>
2021-12-24 13:35:53 +0530

    Update SbkTemplate
    Signed-off-by: Keshava Munegowda <keshava.gowda@gmail.com>

--- driver-sbktemplate/src/main/java/io/sbk/SbkTemplate/SbkTemplate.java
+++ driver-sbktemplate/src/main/java/io/sbk/SbkTemplate/SbkTemplate.java
@@ -46,11 +46,15 @@ public class SbkTemplate implements Storage<byte[]> {
             throw new IllegalArgumentException(ex);
         }
 
+        // change and uncomment the below code as per your driver specific parameters
+        // params.addOption("param", true, "SbkTemplate parameter, default param: " + config.param);
         throw new IllegalArgumentException("The SbkTemplate Driver not defined");
     }
 
     @Override
     public void parseArgs(final ParameterOptions params) throws IllegalArgumentException {
+        // change and uncommnet the below code as per your driver specific parameters
+        // config.param = params.getOptionValue("param", config.param);
         throw new IllegalArgumentException("The SbkTemplate Driver not defined");
     }
 
