24deb80b4ec475cc778a8b950061c486d404da58
Keshava Munegowda <keshava.gowda@gmail.com>
2022-01-01 18:47:54 +0530

    SBK-RAM javadoc cleanup
    Signed-off-by: Keshava Munegowda <keshava.gowda@gmail.com>

--- sbk-ram/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.sbk.config"},{"l":"io.sbk.logger"},{"l":"io.sbk.logger.impl"},{"l":"io.sbk.main"},{"l":"io.sbk.ram"},{"l":"io.sbk.ram.impl"}];updateSearchResults();
