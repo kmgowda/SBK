bffb66b9575649c7f1e71e516003dde4bf59ea40
Keshava Munegowda <keshava.gowda@gmail.com>
2022-01-01 18:53:37 +0530

    SBK-GEM-YAL cleanup.
    Signed-off-by: Keshava Munegowda <keshava.gowda@gmail.com>

--- sbk-gem-yal/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.sbk.gem.impl"},{"l":"io.sbk.main"},{"l":"io.sbk.yal.impl"}];updateSearchResults();
--- sbk-gem-yal/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"io.sbk.gem.impl","l":"SbkGemYal"},{"p":"io.sbk.main","l":"SbkGemYalMain"},{"p":"io.sbk.gem.impl","l":"SbkGemYalParameters"},{"p":"io.sbk.yal.impl","l":"SbkGemYmlMap"}];updateSearchResults();
