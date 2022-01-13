8009e373bcf9efaa844ec071d827504bec7ce60a
Keshava Munegowda <keshava.gowda@gmail.com>
2022-01-13 20:24:09 +0530

    Cleanup
    Signed-off-by: Keshava Munegowda <keshava.gowda@gmail.com>

--- charts/sbk-charts.py
+++ charts/sbk-charts.py
@@ -13,13 +13,22 @@
 import sys
 import argparse
 
+
+class SbkCharts:
+  def __init__(self, iFile, oFile):
+    self.iFile = iFile
+    self.oFile = oFile
+
+
+
 def main():
    parser = argparse.ArgumentParser(description='sbk charts')
    parser.add_argument('-i','--ifile', help='Input CSV file', required=True)
    parser.add_argument('-o','--ofile', help='Output xlsx file',  default="out.xlsx")
    args = parser.parse_args()
-   print('Input file is ', args.ifile)
-   print('Output file is ', args.ofile)
+   chart = SbkCharts(args.ifile, args.ofile)
+   print('Input file is ', chart.iFile)
+   print('Output file is ', chart.oFile)
 
 if __name__ == "__main__":
    main()
