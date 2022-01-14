5736f142bd076172dfc70f5d5c5702625a326851
Keshava Munegowda <keshava.gowda@gmail.com>
2022-01-14 18:37:28 +0530

    Add percentile_10 graph
    Signed-off-by: Keshava Munegowda <keshava.gowda@gmail.com>

--- charts/sbk-charts.py
+++ charts/sbk-charts.py
@@ -11,8 +11,12 @@
 # SBK-Charts :  Storage Benchmark Kit - Charts
 
 import argparse
+
+import openpyxl
 import pandas
 import xlsxwriter
+from openpyxl.chart import LineChart, Reference
+from openpyxl.utils import get_column_letter
 
 
 class SbkCharts:
@@ -37,7 +41,7 @@ class SbkCharts:
         for row in df.iterrows():
             if row[1]['Type'] == 'Total':
                 for c, h in enumerate(header):
-                    col_size = len(str(row[1][h]))+1
+                    col_size = len(str(row[1][h])) + 1
                     if col_size > len(h):
                         ws2.set_column(c, c, col_size)
                     ws2.write(r2, c, row[1][h])
@@ -52,6 +56,50 @@ class SbkCharts:
         wb.close()
         print("xlsx file %s created" % self.oFile)
 
+    def get_columns_from_worksheet(self, ws):
+        return {
+            cell.value: {
+                'letter': get_column_letter(cell.column),
+                'number': cell.column
+            } for cell in ws[1] if cell.value
+        }
+
+    def create_graphs(self):
+        self.create_sheets()
+        wb = openpyxl.load_workbook(self.oFile)
+        ws1 = wb["R-1"]
+        ws2 = wb["T-1"]
+        """
+        for row in ws1.iter_rows():
+            lt = []
+            for cell in row:
+                lt.append(cell.value)
+            print(lt)
+        """
+        #print(self.get_columns_from_worksheet(ws1))
+        colnames = self.get_columns_from_worksheet(ws1)
+        values = Reference(ws1, min_col=colnames['Percentile_10']['number'], min_row=2, max_col=colnames['Percentile_10']['number'], max_row=ws1.max_row)
+        chart = LineChart()
+        # adding data to the Bar chart object
+        chart.add_data(values)
+
+        # set the title of the chart
+        chart.title = " Line chart "
+
+        # set the title of the x-axis
+        chart.x_axis.title = " X_AXIS "
+
+        # set the title of the y-axis
+        chart.y_axis.title = " Y_AXIS "
+
+        # add chart to the sheet
+        # the top-left corner of a chart
+        # is anchored to cell E2 .
+        ws3 = wb.create_sheet("Percentile_10")
+        ws3.add_chart(chart)
+        wb.save(self.oFile)
+
+
 
 def main():
     parser = argparse.ArgumentParser(description='sbk charts')
@@ -61,7 +109,7 @@ def main():
     charts = SbkCharts(args.ifile, args.ofile)
     print('Input file is ', charts.iFile)
     print('Output file is ', charts.oFile)
-    charts.create_sheets()
+    charts.create_graphs()
 
 
 if __name__ == "__main__":
