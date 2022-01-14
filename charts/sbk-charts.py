9673e4be05582f3c33b4f4c935b68f9fff791aee
Keshava Munegowda <keshava.gowda@gmail.com>
2022-01-14 12:25:11 +0530

    Split the row based on 'Total' or 'Regular'
    Signed-off-by: Keshava Munegowda <keshava.gowda@gmail.com>

--- charts/sbk-charts.py
+++ charts/sbk-charts.py
@@ -10,25 +10,48 @@
 
 # SBK-Charts :  Storage Benchmark Kit - Charts
 
-import sys
 import argparse
+import pandas
+from openpyxl import Workbook
 
 
 class SbkCharts:
-  def __init__(self, iFile, oFile):
-    self.iFile = iFile
-    self.oFile = oFile
+    def __init__(self, iFile, oFile):
+        self.iFile = iFile
+        self.oFile = oFile
 
+    def create_sheets(self):
+        df = pandas.read_csv(self.iFile)
+        header = df.columns.tolist()
+        wb = Workbook()
+        ws1 = wb.active
+        ws1.title = "Regular"
+        ws2 = wb.create_sheet()
+        ws2.title = "Total"
+        ws1.append(header)
+        ws2.append(header)
+        for row in df.iterrows():
+            lt = list()
+            for h in header:
+                lt.append(row[1][h])
+            if row[1]['Type'] == 'Total':
+                ws2.append(lt)
+            else:
+                ws1.append(lt)
+        wb.save(self.oFile)
+        print("xlsx file %s created" % self.oFile)
 
 
 def main():
-   parser = argparse.ArgumentParser(description='sbk charts')
-   parser.add_argument('-i','--ifile', help='Input CSV file', required=True)
-   parser.add_argument('-o','--ofile', help='Output xlsx file',  default="out.xlsx")
-   args = parser.parse_args()
-   chart = SbkCharts(args.ifile, args.ofile)
-   print('Input file is ', chart.iFile)
-   print('Output file is ', chart.oFile)
+    parser = argparse.ArgumentParser(description='sbk charts')
+    parser.add_argument('-i', '--ifile', help='Input CSV file', required=True)
+    parser.add_argument('-o', '--ofile', help='Output xlsx file', default="out.xlsx")
+    args = parser.parse_args()
+    charts = SbkCharts(args.ifile, args.ofile)
+    print('Input file is ', charts.iFile)
+    print('Output file is ', charts.oFile)
+    charts.create_sheets()
+
 
 if __name__ == "__main__":
-   main()
+    main()
