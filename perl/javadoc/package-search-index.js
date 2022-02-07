fd3c03270d8cd65945c19a556360ad779cfba3d0
Keshava Munegowda <keshava.gowda@gmail.com>
2022-02-07 18:45:07 +0530

    Update javadocs
    Signed-off-by: Keshava Munegowda <keshava.gowda@gmail.com>

--- perl/javadoc/package-search-index.js
+++ perl/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.perl"},{"l":"io.perl.impl"},{"l":"io.state"},{"l":"io.time"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.perl.api"},{"l":"io.perl.api.impl"},{"l":"io.perl.logger"},{"l":"io.perl.logger.impl"},{"l":"io.perl.system"},{"l":"io.state"},{"l":"io.time"}];updateSearchResults();
