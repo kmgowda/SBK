8744892945e81f5d5e39c2c77dfcdefc8df0319a
Keshava Munegowda <keshava.gowda@gmail.com>
2022-03-18 11:35:18 +0530

    Add the couchbase non-functional draft driver
    Signed-off-by: Keshava Munegowda <keshava.gowda@gmail.com>

---
+++ driver-couchbase/src/main/java/io/sbk/Couchbase/CouchbaseReader.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) KMG. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.sbk.Couchbase;
+
+import io.sbk.api.ParameterOptions;
+import io.sbk.api.Reader;
+
+import java.io.IOException;
+
+/**
+ * Class for Couchbase Reader.
+ */
+public class CouchbaseReader implements Reader<byte[]> {
+
+    public CouchbaseReader(int readerId, ParameterOptions params, CouchbaseConfig config) {
+    }
+
+    @Override
+    public byte[] read() throws IOException {
+        throw new IOException("The Couchbase Reader Driver not defined");
+    }
+
+    @Override
+    public void close() throws IOException {
+        throw new IOException("The Couchbase Reader Driver not defined");
+    }
+}
