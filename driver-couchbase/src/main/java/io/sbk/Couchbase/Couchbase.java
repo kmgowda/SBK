bb9931f6e2512cb0588a66dfd1991677e7f6d8c2
sanjay <sanjaynv@gmail.com>
2022-08-27 12:01:18 +0530

    Adding missing files to the repository

--- driver-couchbase/src/main/java/io/sbk/Couchbase/Couchbase.java
+++ driver-couchbase/src/main/java/io/sbk/Couchbase/Couchbase.java
@@ -1,45 +1,37 @@
 /**
  * Copyright (c) KMG. All Rights Reserved..
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
  */
 package io.sbk.Couchbase;
 
-import com.couchbase.client.java.manager.bucket.BucketType;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.javaprop.JavaPropsFactory;
-import com.couchbase.client.core.error.BucketNotFoundException;
-import com.couchbase.client.java.Cluster;
-import com.couchbase.client.java.manager.bucket.BucketManager;
-import com.couchbase.client.java.manager.bucket.BucketSettings;
 import io.sbk.api.DataReader;
 import io.sbk.api.DataWriter;
+import io.sbk.params.ParameterOptions;
 import io.sbk.api.Storage;
 import io.sbk.data.DataType;
-import io.sbk.data.impl.SbkString;
+import io.sbk.data.impl.ByteArray;
 import io.sbk.params.InputOptions;
-import io.sbk.params.ParameterOptions;
-import io.sbk.system.Printer;
-
 
 import java.io.IOException;
-import java.time.Duration;
 import java.util.Objects;
-
 
 /**
  * Class for Couchbase storage driver.
+ *
+ * Incase if your data type in other than byte[] (Byte Array)
+ * then change the datatype and getDataType.
  */
-public class Couchbase implements Storage<String> {
-    private final static String CONFIGFILE = "couchbase.properties";
+public class Couchbase implements Storage<byte[]> {
+    private final static String CONFIGFILE = "Couchbase.properties";
     private CouchbaseConfig config;
-
-    private Cluster cluster;
 
     @Override
     public void addArgs(final InputOptions params) throws IllegalArgumentException {
@@ -54,66 +46,40 @@ public class Couchbase implements Storage<String> {
             throw new IllegalArgumentException(ex);
         }
 
-        params.addOption("url", true, "Database Cluster url, default url: " + config.url);
-        params.addOption("user", true, "User Name, default : " + config.user);
-        params.addOption("password", true, "Bucket password, default : " + config.password);
-        params.addOption("bucket", true, "Bucket Name, default : " + config.bucket);
+        // change and uncomment the below code as per your driver specific parameters
+        // params.addOption("param", true, "Couchbase parameter, default param: " + config.param);
+        throw new IllegalArgumentException("The Couchbase Driver not defined");
     }
 
     @Override
     public void parseArgs(final ParameterOptions params) throws IllegalArgumentException {
-        config.url = params.getOptionValue("url", config.url);
-        config.user = params.getOptionValue("user", config.user);
-        config.password = params.getOptionValue("password", config.password);
-        config.bucket = params.getOptionValue("bucket", config.bucket);
-    }
-
-    public static long generateStartKey(int id) {
-        return (long) id * (long) Integer.MAX_VALUE;
+        // change and uncommnet the below code as per your driver specific parameters
+        // config.param = params.getOptionValue("param", config.param);
+        throw new IllegalArgumentException("The Couchbase Driver not defined");
     }
 
     @Override
     public void openStorage(final ParameterOptions params) throws IOException {
-        cluster = Cluster.connect(config.url, config.user, config.password);
-        BucketManager bucketManager = cluster.buckets();
-        try {
-            if (params.getWritersCount() > 0) {
-                bucketManager.dropBucket(config.bucket);
-            }
-        } catch (BucketNotFoundException ex) {
-            Printer.log.info("The bucket : " + config.bucket + " not found");
-        }
-        Printer.log.info("Bucket List : " +bucketManager.getAllBuckets().keySet());
-        if (bucketManager.getAllBuckets().keySet().contains(config.bucket)) {
-            return;
-        }
-        bucketManager.createBucket(BucketSettings.create(config.bucket)
-                .bucketType(BucketType.COUCHBASE)
-                .ramQuotaMB(120)
-                .numReplicas(1)
-                .replicaIndexes(true)
-                .flushEnabled(true));
-        cluster.bucket(config.bucket).waitUntilReady(Duration.ofSeconds(30));
-
+        throw new IOException("The Couchbase Driver not defined");
     }
 
     @Override
     public void closeStorage(final ParameterOptions params) throws IOException {
-            cluster.disconnect();
+        throw new IOException("The Couchbase Driver not defined");
     }
 
     @Override
-    public DataWriter<String> createWriter(final int id, final ParameterOptions params) {
-        return new CouchbaseWriter(id, params, config, cluster.bucket(config.bucket));
+    public DataWriter<byte[]> createWriter(final int id, final ParameterOptions params) {
+        return new CouchbaseWriter(id, params, config);
     }
 
     @Override
-    public DataReader<String> createReader(final int id, final ParameterOptions params) {
-        return new CouchbaseReader(id, params, config, cluster.bucket(config.bucket));
+    public DataReader<byte[]> createReader(final int id, final ParameterOptions params) {
+        return new CouchbaseReader(id, params, config);
     }
 
     @Override
-    public DataType<String> getDataType() {
-        return new SbkString();
+    public DataType<byte[]> getDataType() {
+        return new ByteArray();
     }
 }
--- driver-couchbase/src/main/java/io/sbk/Couchbase/CouchbaseWriter.java
+++ driver-couchbase/src/main/java/io/sbk/Couchbase/CouchbaseWriter.java
@@ -1,18 +1,16 @@
 /**
  * Copyright (c) KMG. All Rights Reserved.
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
  */
 package io.sbk.Couchbase;
 
-import com.couchbase.client.java.Bucket;
-import com.couchbase.client.java.Collection;
-import io.sbk.api.Writer;
 import io.sbk.params.ParameterOptions;
+import io.sbk.api.Writer;
 
 import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
@@ -21,30 +19,23 @@ import java.util.concurrent.CompletableFuture;
 /**
  * Class for Couchbase Writer.
  */
-public class CouchbaseWriter implements Writer<String> {
+public class CouchbaseWriter implements Writer<byte[]> {
 
-    private Collection collection;
-    final private ParameterOptions params;
-
-    private long key;
-
-    public CouchbaseWriter(int writerID, ParameterOptions params, CouchbaseConfig config, Bucket bucket) {
-        this.key = Couchbase.generateStartKey(writerID);
-        this.params = params;
-        collection = bucket.defaultCollection();
+    public CouchbaseWriter(int writerID, ParameterOptions params, CouchbaseConfig config) {
     }
 
     @Override
-    public CompletableFuture writeAsync(String data) throws IOException {
-        collection.upsert(Long.toString(key++), data);
-        return null;
+    public CompletableFuture writeAsync(byte[] data) throws IOException {
+        throw new IOException("The Couchbase Writer Driver not defined");
     }
 
     @Override
     public void sync() throws IOException {
+        throw new IOException("The Couchbase Writer Driver not defined");
     }
 
     @Override
     public void close() throws IOException {
+        throw new IOException("The Couchbase Writer Driver not defined");
     }
 }
