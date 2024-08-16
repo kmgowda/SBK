9e048585ecdd6e1e085445b62904afbf1dc23ba3
Keshava Munegowda <keshava.gowda@gmail.com>
2024-08-16 19:55:02 +0530

    Implement PerformanceRecorderIdleWait.
    Signed-off-by: Keshava Munegowda <keshava.gowda@gmail.com>

--- perl/src/main/java/io/perl/api/impl/CQueuePerl.java
+++ perl/src/main/java/io/perl/api/impl/CQueuePerl.java
@@ -32,7 +32,7 @@ import java.util.concurrent.ExecutorService;
  * Class for Concurrent Queue based PerL.
  */
 final public class CQueuePerl implements Perl {
-    final private PerformanceRecorder perlReceiver;
+    final private PerformanceRecorderIdleWait perlReceiver;
     final private Channel[] channels;
     final private Time time;
     final private ExecutorService executor;
@@ -75,7 +75,7 @@ final public class CQueuePerl implements Perl {
         for (int i = 0; i < channels.length; i++) {
             channels[i] = new CQueueChannel(maxQs, new OnError());
         }
-        this.perlReceiver = new PerformanceRecorder(periodicRecorder, channels, time, reportingIntervalMS,
+        this.perlReceiver = new PerformanceRecorderIdleWait(periodicRecorder, channels, time, reportingIntervalMS,
                 Math.max(PerlConfig.MIN_IDLE_NS, perlConfig.idleNS));
     }
 
