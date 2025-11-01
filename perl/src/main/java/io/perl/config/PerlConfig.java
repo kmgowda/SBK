82c8edd064916f4af40b34ee82c9b442c20445d5
Keshava Munegowda <keshava.gowda@gmail.com>
2025-11-01 17:42:57 +0530

    Add the documentation to perl
    Signed-off-by: Keshava Munegowda <keshava.gowda@gmail.com>

--- perl/src/main/java/io/perl/config/PerlConfig.java
+++ perl/src/main/java/io/perl/config/PerlConfig.java
@@ -7,7 +7,6 @@
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  */
-
 package io.perl.config;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -21,6 +20,11 @@ import java.util.Objects;
 
 /**
  * Class PerlConfig extending LatencyConfig.
+ *
+ * PerL runtime configuration that augments {@link LatencyConfig} with worker/queue
+ * parameters and timing defaults used by the PerL benchmark/runtime. Instances are
+ * typically populated from the {@code perl.properties} file on the classpath via
+ * the static {@link #build()} helpers.
  */
 final public class PerlConfig extends LatencyConfig {
 
@@ -28,72 +32,82 @@ final public class PerlConfig extends LatencyConfig {
      * <code>String NAME = "PerL"</code>.
      */
     final public static String NAME = "PerL";
-
-
     /**
+     * Default interval (in seconds) between periodic status/metrics prints.
      * <code>int DEFAULT_PRINTING_INTERVAL_SECONDS = 5</code>.
      */
     final public static int DEFAULT_PRINTING_INTERVAL_SECONDS = 5;
 
     /**
+     * Default timeout (in milliseconds) for internal operations.
      * <code>int DEFAULT_TIMEOUT_MS = Time.MS_PER_SEC</code>.
      */
     final public static int DEFAULT_TIMEOUT_MS = Time.MS_PER_SEC;
 
     /**
+     * Default maximum runtime (in seconds) if not otherwise limited.
      * <code>long DEFAULT_RUNTIME_SECONDS = Long.MAX_VALUE / Time.MS_PER_SEC</code>.
      */
     final public static long DEFAULT_RUNTIME_SECONDS = Long.MAX_VALUE / Time.MS_PER_SEC;
 
     /**
+     * Minimum number of worker threads allowed.
      * <code>int MIN_WORKERS = 1</code>.
      */
     final public static int MIN_WORKERS = 1;
 
     /**
+     * Minimum number of queues per worker.
      * <code>int MIN_Q_PER_WORKER = 3</code>.
      */
     final public static int MIN_Q_PER_WORKER = 3;
 
     /**
+     * Minimum idle time (in nanoseconds) used to yield between operations.
      * <code>int MIN_IDLE_NS = Time.NS_PER_MICRO</code>.
      */
     final public static int MIN_IDLE_NS = Time.NS_PER_MICRO;
 
 
     /**
+     * Default configuration file name from which to load PerL settings.
      * <code>String CONFIGFILE = "perl.properties"</code>.
      */
     final private static String CONFIGFILE = "perl.properties";
 
     /**
+     * Number of worker threads to run.
      * <code>int workers</code>.
      */
     public int workers;
 
     /**
+     * Number of queues assigned per worker.
      * <code>int qPerWorker</code>.
      */
     public int qPerWorker;
 
     /**
+     * Idle time between operations (in nanoseconds).
      * <code>int idleNS</code>.
      */
     public int idleNS;
 
     /**
+     * Sleep interval between loops (in milliseconds), when applicable.
      * <code>int sleepMS</code>.
      */
     public int sleepMS;
 
     /**
+     * Maximum number of queues allowed.
      * <code>int maxQs</code>.
      */
     public int maxQs;
 
 
     /**
-     * Method build with no arguments.
+     * Build configuration by loading the default {@code perl.properties} from the classpath.
      *
      * @return PerlConfig - build(PerlConfig.class.getClassLoader().getResourceAsStream(CONFIGFILE));
      * @throws IOException  If it occurs.
@@ -103,7 +117,7 @@ final public class PerlConfig extends LatencyConfig {
     }
 
     /**
-     * Method build with arguments.
+     * Build configuration by reading properties from the given input stream.
      *
      * @param in            InputStream
      * @return PerlConfig - mapper.readValue(Objects.requireNonNull(in), PerlConfig.class);
